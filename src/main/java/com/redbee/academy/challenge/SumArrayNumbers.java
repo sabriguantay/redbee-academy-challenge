7a2d8abaa2f7ca1cd879de51996c2b90832cd8f9
Sabrina Guantay <52327724+sabriguantay@users.noreply.github.com>
2021-06-21 19:46:22 -0300

    Update SumArrayNumbers.java

--- src/main/java/com/redbee/academy/challenge/SumArrayNumbers.java
+++ src/main/java/com/redbee/academy/challenge/SumArrayNumbers.java
@@ -11,8 +11,13 @@ public class SumArrayNumbers {
    * @param array - Some integer list. ([1,2,3])
    * @return The result of 1 + 2 + 3
    */
+
   public static Integer sum(List<Integer> array) {
-    //TODO: Implement me
-    return null;
+    Integer suma = 0;
+ 
+    for (Integer i : array){
+      suma += i;
+    }
+    return suma;
   }
 }
